feee0c1c4bee7dc75214956f9364f1a2f8525f8c
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2019-03-16 23:43:24 -0300

    Update sample_List.c

--- samples/sample_List.c
+++ samples/sample_List.c
@@ -1,42 +1,43 @@
-	#include <stdio.h>
-	#include "wizard_by_list.h"
+#include <stdio.h>
+#include "wizard_by_list.h"
 
-	int f1(void *a){
-	   /* do some thing... */
-	   printf("[%s]\n", (char *)a);
-	   return(1); 
-	}
+int f1(void *a){
+	/* do some thing... */
+	printf("[%s]\n", (char *)a);
+	return(1); 
+}
 
-	int f2(void *a){
-	   /* do some thing... */
-	   printf("[%s]\n", (char *)a);
-	   return(1);  
-	}
+int f2(void *a){
+	/* do some thing... */
+	printf("[%s]\n", (char *)a);
+	return(1);  
+}
 
-	int f3(void *a){
-	   /* do some thing... */
-	   printf("[%s]\n", (char *)a);
-	   return(-1); /* ERROR Here!!! */
-	}
+int f3(void *a){
+	/* do some thing... */
+	printf("[%s]\n", (char *)a);
+	return(-1); /* ERROR Here!!! */
+}
 
-	int f4(void *a){
-	   /* do some thing... */
-	   printf("[%s]\n", (char *)a);
-	   return(-1); 
-	}
+int f4(void *a){
+	/* do some thing... */
+	printf("[%s]\n", (char *)a);
+	return(-1); 
+}
 
-	int main(){
-	   char buff[10] = "abc";
-	   unsigned int step = 0;
-	   int last_ret = 0;
+int main()
+{
+	char buff[10] = "abc";
+	unsigned int step = 0;
+	int last_ret = 0;
 
-	   a2gs_ToolBox_WizardListFunc_t list[] = {f1, f2, f3, f4, NULL};
+	a2gs_ToolBox_WizardListFunc_t list[] = {f1, f2, f3, f4, NULL};
 
-	   last_ret = a2gs_ToolBox_WizardList(list, buff, -1, &step);
+	last_ret = a2gs_ToolBox_WizardList(list, buff, -1, &step);
 
-	   if(list[step] == NULL)
-	      printf("Executed all functions\n");
-	   printf("Executation goes to step: [%d] and return [%d]\n", step, last_ret);
+	if(list[step] == NULL)
+		printf("Executed all functions\n");
+	printf("Executation goes to step: [%d] and return [%d]\n", step, last_ret);
 
-	   return(1);
-	}
+	return(1);
+}
